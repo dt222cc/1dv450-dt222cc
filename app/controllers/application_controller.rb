d29d7d035103056f153b2a11b66d01df8b93ded8
Sing Trinh <dt222cc@student.lnu.se>
2016-02-09 05:08:58 +0000

    Fix: Prevents user from accessing previous page by backing, when an user has logged out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  
-  # We must include the helper we want to user
-  include SessionsHelper
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+
+    # We must include the helper we want to user
+    include SessionsHelper
+
+    # No cache, one solution to not re login after user has been logged out by backing
+    before_action :no_cache
+
+    def no_cache
+        response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "Fri, 09 Jan 2004 00:00:00 GMT"
+    end
 end
