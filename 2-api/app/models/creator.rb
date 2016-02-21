41bd14d870477d378230d5c0b8a0a152520196fb
Sing Trinh <dt222cc@student.lnu.se>
2016-02-21 18:27:33 +0000

    UPDATE new and previous models | :large_orange_diamond:
    Updated API models.
    
    Updated comments and indentation of registration models

--- 2-api/app/models/creator.rb
+++ 2-api/app/models/creator.rb
@@ -1,2 +1,16 @@
 class Creator < ActiveRecord::Base
+  # A creator creates events which are connected to the user
+  has_many :events
+
+  # Lowercase email before saving to db
+  before_save { self.email = email.downcase }
+
+  # Validates username and email for presence, email format for the email
+  # Validates the uniqueness only on creation, also not case sensitive
+  validates :username, presence: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/ }
+  validates :email, uniqueness: { case_sensitive: false }, on: :create
+
+  # Using the bcrypt gem, has some built in validation
+  has_secure_password
 end
--- 2-api/app/models/tag.rb
+++ 2-api/app/models/tag.rb
@@ -1,2 +1,7 @@
 class Tag < ActiveRecord::Base
+  # A tag is associated with events and thus the has_and_belongs_to_many
+  has_and_belongs_to_many :events
+
+  # Validates name for presence
+  validates :name, presence: true
 end
