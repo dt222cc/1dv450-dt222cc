2aa252c57f75d9f36cf0aa381b247fb8450610d9
Sing Trinh <dt222cc@student.lnu.se>
2016-02-21 23:35:56 +0000

    UPDATE: API models, "index" in json | :large_orange_diamond:

--- 2-api/app/controllers/api/v1/creators_controller.rb
+++ 2-api/app/controllers/api/v1/creators_controller.rb
@@ -1,2 +1,5 @@
 class Api::V1::CreatorsController < ApplicationController
+  def index
+    render json: Creator.all
+  end
 end
--- 2-api/app/controllers/api/v1/positions_controller.rb
+++ 2-api/app/controllers/api/v1/positions_controller.rb
@@ -1,2 +1,5 @@
 class Api::V1::PositionsController < ApplicationController
+  def index
+    render json: Position.all
+  end
 end
