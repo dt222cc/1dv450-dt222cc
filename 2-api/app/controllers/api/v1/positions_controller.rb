d38cbe72234e29be8056de340ea74bd33ded20e4
Sing Trinh <dt222cc@student.lnu.se>
2016-02-23 20:11:41 +0000

    UPDATE: positions_controller.rb with Create and Delete | :white_check_mark:

--- 2-api/app/controllers/api/v1/positions_controller.rb
+++ 2-api/app/controllers/api/v1/positions_controller.rb
@@ -23,4 +23,52 @@ class Api::V1::PositionsController < Api::V1::ApiController
       render json: position, status: :ok
     end
   end
+
+  # POST /api/v1/positions
+  # Handle request in wrong format, must be numeric, not duplicate
+  def create
+    begin
+      position = params.require(:position).permit(:latitude, :longitude)
+    rescue
+      render json: { error: 'Wrong format', position: { latitude: 'integer', longitude: 'integer' } }, status: :bad_request and return
+    end
+
+    if !(numeric?(position['latitude']) && numeric?(position['longitude']))
+      render json: { error: 'Not numeric!'}, status: :bad_request
+    else
+      position = Position.new(position)
+      exists = Position.find_by_latitude_and_longitude(position.latitude, position.longitude)
+      if exists
+        render json: { error: 'Position already exists', position: exists}, status: :conflict
+      elsif position.save
+        render json: position, status: :created
+      else
+        render json: position.errors.messages, status: :unprocessable_entity # If something odd happens
+        # render json: position.errors, status: :unprocessable_entity
+        # render json: { errors: 'Unexpected error occured, contact support' }, status: :unprocessable_entity
+      end
+    end
+  end
+
+  # DELETE /api/v1/positions/:id
+  def destroy
+    position = Position.find_by_id(params[:id])
+
+    # If the position exists > delete/destroy
+    if !position.nil?
+      position.destroy
+      head :no_content # http://stackoverflow.com/questions/8592921/how-to-return-http-204-in-a-rails-controller
+    else
+      render json: { error: 'Position was not found' }, status: :not_found
+    end
+  end
+
+  private
+  # Source: http://stackoverflow.com/questions/26061584/how-do-i-determine-if-a-string-is-numeric
+  def numeric?(string)
+    # `!!` converts parsed number to `true`
+    !!Kernel.Float(string)
+  rescue TypeError, ArgumentError
+    false
+  end
 end
