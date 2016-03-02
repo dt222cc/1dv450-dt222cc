a814a235104bef748d9307568321e3a9846e912d
Sing Trinh <dt222cc@student.lnu.se>
2016-03-02 19:51:34 +0000

    Refactor: 2-api/config/routes.rb
    Test

--- 2-api/config/routes.rb
+++ 2-api/config/routes.rb
@@ -13,25 +13,23 @@ Rails.application.routes.draw do
   resources :apps
 
   # Resource route for api, within namespaces
-  namespace :api, defaults: {format: 'json'}  do
+  namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      # resources :creators, :events, :positions, :tags
-
       resources :creators, only: [:index, :show] do
-        resources :events, only: [:index, :show]
+        resources :events, only: [:index]
       end
 
       resources :events, only: [:index, :show, :create, :destroy, :update] do
-        resources :positions, only: [:index, :show]
-        resources :tags, only: [:index, :show]
+        resources :positions, only: [:index]
+        resources :tags, only: [:index]
       end
 
       resources :positions, only: [:index, :show] do
-        resources :events, only: [:index, :show]
+        resources :events, only: [:index]
       end
 
       resources :tags, only: [:index, :show] do
-        resources :events, only: [:index, :show]
+        resources :events, only: [:index]
       end
     end
   end
