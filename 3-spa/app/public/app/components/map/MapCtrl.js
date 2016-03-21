ff14216e17581111c1e7bb3ec28a9317e4cf54e7
Sing Trinh <dt222cc@student.lnu.se>
2016-03-21 17:02:18 +0100

    Add map markers and info-window for events

--- 3-spa/app/public/app/components/map/MapCtrl.js
+++ 3-spa/app/public/app/components/map/MapCtrl.js
@@ -1,10 +1,23 @@
 /**
- *
+ * // https://github.com/allenhwkim/angularjs-google-maps
  */
-positioningApp.controller('MapController', function($scope, NgMap) {
-  console.log('inside MapController');
-  NgMap.getMap(map).then(function(map) { // Have to include map here to find the map..
-    console.log('map retrieved');
-    console.log(map);
+positioningApp.controller('MapController', ['$scope', 'NgMap', function($scope, NgMap) {
+  var vm = this;
+
+  NgMap.getMap().then(function(map) {
+    vm.map = map;
   });
-});
+
+  /**
+   * Pan to event, set new zoom(closer), show info window
+   * @param  {[type]} e     [required]
+   * @param  {[type]} event [the event]
+   */
+  $scope.showEvent = function(e, event) {
+    vm.event = event;
+    var eventPosition = new google.maps.LatLng(event.position.latitude, event.position.longitude);
+    vm.map.setCenter(eventPosition);
+    vm.map.setZoom(10);
+    vm.map.showInfoWindow('eventInfo', this);
+  };
+}]);
