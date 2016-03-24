dc5fcd61c8ca4e18940f592a6ef4d1dd0c9b946f
Sing Trinh <dt222cc@student.lnu.se>
2016-03-24 17:50:08 +0100

    Add myKeyPress.js | directive

---
+++ 3-spa/app/public/app/directives/myKeyPress.js
@@ -0,0 +1,16 @@
+/**
+ * Usage: Add attribute 'my-key-code' in elements that should track keypresses,
+ * include attribute 'code' with the key code it should accept
+ */
+positioningApp.directive('myKeyPress', function() {
+  return function(scope, element, attrs) {
+    element.bind("keypress", function(event) {
+      var keyCode = event.which || event.keyCode;
+      if (keyCode == attrs.code) { // Check if keycode match
+        scope.$apply(function() {
+          scope.$eval(attrs.myKeyPress); // Evaluate the expression
+        });
+      }
+    });
+  };
+});
