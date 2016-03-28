a3614b244528f5f4fa70441f2c3d4c6a4f4899c4
Sing Trinh <dt222cc@student.lnu.se>
2016-03-28 21:04:42 +0200

    Update (minor)

--- 3-spa/app/public/app/directives/myKeyPress.js
+++ 3-spa/app/public/app/directives/myKeyPress.js
@@ -3,14 +3,17 @@
  * include attribute 'code' with the key code it should accept
  */
 positioningApp.directive('myKeyPress', function() {
-  return function(scope, element, attrs) {
-    element.bind("keypress", function(event) {
-      var keyCode = event.which || event.keyCode;
-      if (keyCode == attrs.code) { // Check if keycode match
-        scope.$apply(function() {
-          scope.$eval(attrs.myKeyPress); // Evaluate the expression
-        });
-      }
-    });
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      element.bind("keypress", function(event) {
+        var keyCode = event.which || event.keyCode;
+        if (keyCode == attrs.code) { // Check if keycode match
+          scope.$apply(function() {
+            scope.$eval(attrs.myKeyPress); // Evaluate the expression
+          });
+        }
+      });
+    }
   };
 });
