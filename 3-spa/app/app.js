f23c74598a0b86f19ce0797873cba56d97cd566e
Sing Trinh <dt222cc@student.lnu.se>
2016-06-03 16:37:16 +0200

    SPA: Update README.md

--- 3-spa/app/app.js
+++ 3-spa/app/app.js
@@ -57,4 +57,5 @@ app.get('*', function(req, res) {
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
+  console.log('URL: localhost:' + app.get('port'));
 });
